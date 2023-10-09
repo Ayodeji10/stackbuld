9c445e84cb5d4e485ddcd74a4873c22b04640421
Ayodeji10 <eniolakayode38@gmail.com>
2023-10-09 08:54:29 +0100

    readme update

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,17 @@ import Home from "./screens/home";
 import SingleTag from "./screens/singleTag";
 import Authors from "./screens/authors";
 import AUthorPost from "./screens/authorPosts";
+import ScrollToTop from "./scrollToTop";
 
 function App() {
   return (
     <Router>
+      {/* scroll page to top on load  */}
+      <ScrollToTop />
+      {/* navbar  */}
       <Navbar />
       <Routes>
+        {/* routes  */}
         <Route path="/" element={<Home />} />
         <Route path="/posts/page/:page" element={<Posts />} />
         <Route path="/posts/:id" element={<SinglePost />} />
